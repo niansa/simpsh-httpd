de7b6224618fab9a8d09e1667df37901dd535b31
niansa <anton-sa@web.de>
2017-08-25 17:05:08 +0200

    Create mkdeb.sh

---
+++ mkdeb.sh
@@ -0,0 +1,13 @@
+cd ~
+git clone https://github.com/niansa/simpsh-httpd.git
+chmod +x ./simpsh-httpd/mkpackage.sh
+./simpsh-httpd/mkpackage.sh ~/simpsh-httpd/ ~/simpsh-httpd-pkg/
+mkdir ~/simpsh-httpd-pkg/DEBIAN
+echo 'Package: simpsh-httpd
+Version: 1.2.0
+Maintainer: nisa/niansa <nisa@bitcoinshell.mooo.com>
+Priority: extra
+Architecture: all
+Depends: bash, socat
+Description: An usable bash-httpserver with easy configuration and very simple PHP' > ~/simpsh-httpd-pkg/DEBIAN/control
+dpkg -b ~/simpsh-httpd-pkg ~/simpsh-httpd.deb
