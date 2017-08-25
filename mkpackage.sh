9b850966da7bb8edfb119c7f15cf361642c91c85
niansa <anton-sa@web.de>
2017-08-25 13:26:11 +0200

    Update mkpackage.sh

--- mkpackage.sh
+++ mkpackage.sh
@@ -1,6 +1,4 @@
 echo "Bitte warten... Please wait..."
-old0="$0"
-mv "$old0" /tmp/mkpackage.sh-$$
 mkdir "$2"
 cd "$2"
 mkdir etc
@@ -16,5 +14,4 @@ echo 'cd /usr/share/simpsh-httpd/
 mv usr/share/simpsh-httpd/config.sh etc/simpsh-httpd
 ln -sf etc/simpsh-httpd/config.sh usr/share/simpsh-httpd/config.sh
 chmod -R +x .
-mv /tmp/mkpackage.sh-$$ "$old0"
 echo "Fertig! Done!"
