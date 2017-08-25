c91418a7eda381bb78f8a6327c8f20616c807c47
Stev <steven.caminada@academia-schweiz.ch>
2017-08-25 16:09:25 +0200

    Update start-socat.sh

--- start-socat.sh
+++ start-socat.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 . ./config.sh
-socat TCP-LISTEN:$PORT,reuseaddr,fork SYSTEM:"./main.sh" || exit 1
 echo "Listening at Port: $PORT"
+socat TCP-LISTEN:$PORT,reuseaddr,fork SYSTEM:"./main.sh" || exit 1
+
