aa8a063dba593d04ffd120ad38f7b459e20388f1
niansa <anton-sa@web.de>
2017-12-26 12:07:59 +0100

    Update mkpackage.sh

--- mkpackage.sh
+++ mkpackage.sh
@@ -2,7 +2,6 @@ echo "Bitte warten... Please wait..."
 mkdir "$2"
 cd "$2"
 mkdir etc
-mkdir etc/init.d
 mkdir etc/simpsh-httpd
 mkdir usr
 mkdir usr/bin
@@ -12,7 +11,6 @@ echo 'cd /usr/share/simpsh-httpd
 ./main.sh' > usr/bin/simpsh-httpd
 echo 'cd /usr/share/simpsh-httpd/
 ./start-socat.sh $@' > usr/bin/simpsh-httpd-socat
-mv usr/share/simpsh-httpd/simpsh-httpd.service etc/init.d/simpsh-httpd.sh
 mv usr/share/simpsh-httpd/config.sh etc/simpsh-httpd
 ln -sf /etc/simpsh-httpd/config.sh usr/share/simpsh-httpd/config.sh
 chmod -R a+x .
