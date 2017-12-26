0cb83592eb637946fb4766e4ba717e47f5ab1a05
niansa <anton-sa@web.de>
2017-12-26 11:22:04 +0100

    Update start-socat.sh

--- start-socat.sh
+++ start-socat.sh
@@ -3,7 +3,7 @@
 . ./config.sh
 echo "Listening at Port: $PORT"
 if [ "$1" = "-d" -o "$1" = "--daemon" ]; then
-  nohup socat TCP-LISTEN:$PORT,reuseaddr,fork SYSTEM:"./main.sh" > /dev/null 2>&1 &
+  setsid socat TCP-LISTEN:$PORT,reuseaddr,fork SYSTEM:"./main.sh" > /dev/null 2>&1 &
 else
   socat TCP-LISTEN:$PORT,reuseaddr,fork SYSTEM:"./main.sh" || exit 1
 fi
