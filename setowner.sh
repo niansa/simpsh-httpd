c7098bd1216d0639627a156477e6d0cc0be6939d
niansa <anton-sa@web.de>
2017-12-26 11:23:53 +0100

    Update setowner.sh

--- setowner.sh
+++ setowner.sh
@@ -1,3 +1,5 @@
 chown -R root:root /usr/share/simpsh-httpd
 chown -R root:root /etc/simpsh-httpd
 chown root:root /etc/init.d/simpsh-httpd.sh
+chown root:root /usr/bin/simpsh-httpd
+chown root:root /usr/bin/simpsh-httpd-socat
