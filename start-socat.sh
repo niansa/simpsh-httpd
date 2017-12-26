124caad5bb352e10943febe32c4788ad39bdf5f8
niansa <anton-sa@web.de>
2017-12-26 11:48:08 +0100

    Update start-socat.sh

--- start-socat.sh
+++ start-socat.sh
@@ -5,5 +5,8 @@ echo "Listening at Port: $PORT"
 if [ "$1" = "-d" -o "$1" = "--daemon" ]; then
   setsid socat TCP-LISTEN:$PORT,reuseaddr,fork SYSTEM:"./main.sh" > /dev/null 2>&1 &
 else
-  socat TCP-LISTEN:$PORT,reuseaddr,fork SYSTEM:"./main.sh" || exit 1
+  socat TCP-LISTEN:$PORT,reuseaddr,fork SYSTEM:"./main.sh" &
+  echo $! > /run/lock/simpsh-httpd.lock
+  fg
+  rm /run/lock/simpsh-httpd.lock
 fi
