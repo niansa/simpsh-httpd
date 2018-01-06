98e1a3f8599f170d041eb7af0077564cc60f9b18
niansa <anton-sa@web.de>
2018-01-06 16:08:17 +0100

    Update start-socat.sh

--- start-socat.sh
+++ start-socat.sh
@@ -2,10 +2,12 @@
 
 echo 'simpsh-httpd  Copyright (C) 2018  niansa
 This program comes with ABSOLUTELY NO WARRANTY; for details read "'"$(pwd)/LICENSE"'".
-This is free software, and you are welcome to redistribute it under certain conditions; read "'"$(pwd)/LICENSE"'" for details.'
+This is free software, and you are welcome to redistribute it under certain conditions; read "'"$(pwd)/LICENSE"'" for details.
+'
 
+test -f ./config.sh || (echo "Configuration file missing!!!" ; exit 1)
 . ./config.sh
-echo "Listening at Port: $PORT"
+echo "Listening at Port $PORT"
 if [ "$1" = "-d" -o "$1" = "--daemon" ]; then
   setsid socat TCP-LISTEN:$PORT,reuseaddr,fork SYSTEM:"./main.sh" > /dev/null 2>&1 &
 else
