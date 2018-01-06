c9e73654beafd3ada62dcebd5b0d3b07724ca004
niansa <anton-sa@web.de>
2018-01-06 16:14:49 +0100

    Update start-socat.sh

--- start-socat.sh
+++ start-socat.sh
@@ -5,11 +5,6 @@ This program comes with ABSOLUTELY NO WARRANTY; for details read "'"$(pwd)/LICEN
 This is free software, and you are welcome to redistribute it under certain conditions; read "'"$(pwd)/LICENSE"'" for details.
 '
 
-if [ -f ./config.sh ]; then
-  echo "Configuration file missing!!!"
-  exit 1
-fi
-
 . ./config.sh
 echo "Listening at Port $PORT"
 if [ "$1" = "-d" -o "$1" = "--daemon" ]; then
