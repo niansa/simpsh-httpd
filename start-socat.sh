2a3d55917f1401ddb88b34f4a04a766053e7dc00
niansa <anton-sa@web.de>
2018-01-06 16:20:05 +0100

    Update start-socat.sh

--- start-socat.sh
+++ start-socat.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 echo 'simpsh-httpd  Copyright (C) 2018  niansa
-This program comes with ABSOLUTELY NO WARRANTY; for details read "'"$(pwd)/LICENSE"'".
-This is free software, and you are welcome to redistribute it under certain conditions; read "'"$(pwd)/LICENSE"'" for details.
+This program comes with ABSOLUTELY NO WARRANTY; for details read "'"$(pwd)/LICENSE.md"'".
+This is free software, and you are welcome to redistribute it under certain conditions; read "'"$(pwd)/LICENSE.md"'" for details.
 '
 
 . ./config.sh
