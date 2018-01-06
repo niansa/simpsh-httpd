a360f6503ae48a929e298bcac8929bace92726a0
niansa <anton-sa@web.de>
2018-01-06 15:58:27 +0100

    Update start-socat.sh

--- start-socat.sh
+++ start-socat.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+echo 'simpsh-httpd  Copyright (C) 2018  niansa
+This program comes with ABSOLUTELY NO WARRANTY; for details read "'"$(pwd)/LICENSE"'".
+This is free software, and you are welcome to redistribute it under certain conditions; read "'"$(pwd)/LICENSE"'" for details.'
+
 . ./config.sh
 echo "Listening at Port: $PORT"
 if [ "$1" = "-d" -o "$1" = "--daemon" ]; then
